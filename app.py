55ad23356bdf62edf5780e50043b4b7cc54c6d39
phucnguyen06 <34689597+phucnguyen06@users.noreply.github.com>
2017-12-24 15:29:35 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,99 @@
-in_stock = [
-    ("t-shirt", "blue", 9.99),
-    ("t-shirt", "white", 12.99),
-    ("shirt", "blue", 19.99),
-    ("shirt", "black", 17.99),
-    ("jean","dark blue",49.99)
-    ]
+from flask import Flask, render_template, request, redirect
+import datetime
+import pytz # timezone 
+import requests
+import os
 
-in_stock_1 = [
-    "t-shirt", "blue", 9.99,
-    "t-shirt", "white", 12.99,
-    "shirt", "blue", 19.99,
-    "shirt", "black", 17.99
-    ]
 
-top = []
-bottom = []
-t_shirt = []
 
-def show_Tshirt():
-    for item in top:
-        if item[0] == 't-shirt':
-            t_shirt.append(top[:2])
-    return t_shirt
+app = Flask(__name__)
 
-def show_top():
-    print("Here's top products: ")
-    for item in top:
-        for thing in item:
-            print(thing)
-        
-def show_bottom():
-    print("Here's bottom products: ")
-    for item in bottom:
-        print(item)
 
-while True:
-    try:
-        search = input("Search: ")
-    except ValueError:
-        pass
-    else:
-        if search == 'top':
-            top.append(in_stock[:4])
-            show_top()
-            continue
-        elif search == 'home':
-            show_top()
-            show_bottom()
-            break
-        elif search == 'bottom':
-            bottom.append(in_stock[-1])
-            show_bottom()
-            continue
-        else:
-            print(False)
+@app.route('/', methods=['GET'])
+def home_page():
+	return render_template('index.html')
 
+@app.route('/<name>')
+def profile(name):
+	return render_template('index.html', name=name)
+
+
+@app.route('/add_numbers', methods=['GET','POST'])
+def add_numbers_post():
+	  # --> ['5', '6', '8']
+	  # print(type(request.form['text']))
+	  if request.method == 'GET':
+	  	return render_template('add_numbers.html')
+	  elif request.method == 'POST':
+  	      print(request.form['text'].split())
+  	      total = 0
+  	      try:
+  	      	for str_num in request.form['text'].split():
+  	      		total += int(str_num)
+  	      	return render_template('add_numbers.html', result=str(total))
+  	      except ValueError:
+  	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
+
+
+@app.route('/shopping_list', methods=['GET','POST'])
+def shopping_list_post():
+	  # --> ['5', '6', '8']
+	  # print(type(request.form['text']))
+
+    if request.method == 'GET':
+      return render_template('shopping_list.html')
+    elif request.method == 'POST':
+          print(request.form['text'].split())
+          
+          shop_list = []
+          try:
+            for item in request.form['text'].split():
+              
+              shop_list.append(item)
+
+              
+              
+            return render_template('shopping_list.html', result="\n".join([str(item) for item in shop_list]))
+          except ValueError:
+            return "Easy now! Let's keep it simple! Just words with a space between them"
+          
+  	      
+@app.route('/time', methods=['GET','POST'])
+def time_post():
+    # --> ['5', '6', '8']
+    # print(type(request.form['text']))
+
+    if request.method == 'GET':
+      return render_template('time.html')
+    elif request.method == 'POST':
+          print(request.form['text'].split())
+          
+          for item in request.form['text'].split():
+            answer = (datetime.datetime.now(pytz.timezone("Europe/Dublin")).strftime('Time = ' + '%H:%M:%S' + ' GMT ' + ' Year = ' + '%d-%m-%Y'))
+            #answer = datetime.datetime.now().strftime('Time == ' + '%H:%M:%S' + ' Year == ' + '%d-%m-%Y')
+            #answer = datetime.datetime.now().strftime('%Y-%m-%d \n %H:%M:%S')
+
+              
+              
+            return render_template('time.html', result=answer)
+
+         
+
+@app.route('/python_apps')
+def python_apps_page():
+	# testing stuff
+	return render_template('python_apps.html')
+
+
+@app.route('/contact')
+def contact():
+	return render_template('contact.html')
+
+@app.route('/blog', methods=['GET'])
+def blog_page():
+  return render_template('blog.html')
+
+app.run(host=os.getenv('IP', '0.0.0.0'), port = int(os.getenv('PORT', 8080)))
+
+if __name__ == '__main__':
+	app.run(debug=False)
