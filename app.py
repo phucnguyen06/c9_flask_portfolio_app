21b49ba21845f8c322f4569c5fa0ba4279c5d72e
phucnguyen06 <34689597+phucnguyen06@users.noreply.github.com>
2017-12-25 20:52:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -20,23 +20,17 @@ def profile(name):
 
 @app.route('/add_numbers', methods=['GET','POST'])
 def add_numbers_post():
-	  # --> ['5', '6', '8']
-	  # print(type(request.form['text']))
-	  if request.method == 'GET':
-	  	return render_template('add_numbers.html')
-	  elif request.method == 'POST':
-  	      print(request.form['text'].split())
-  	      words = {}
-  	      try:
-  	      	for word in request.form['text'].split():
-			if word in words:
-				words[word] += 1
-			else:
-				words[word] = 1
-  	      		
-  	      	return render_template('add_numbers.html', result=str(words))
-  	      except ValueError:
-  	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
+	if request.method == 'GET':
+		return render_template('add_numbers.html')
+	elif request.method == 'POST':
+		print(request.form['text'].split())
+	total = 0
+	try:
+		for str_num in request.form['text'].split():
+			total += int(str_num)
+		return render_template('add_numbers.html', result=str(total))
+	except ValueError:
+		return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
 
 
 @app.route('/shopping_list', methods=['GET','POST'])
